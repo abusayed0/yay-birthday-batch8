07fb22b292d8bc17cefe539950d7daaf605ad4b1
abusayed0 <abusayed48876@gmail.com>
2023-10-11 15:43:22 +0600

    added spinner when user user is loding in protect page

--- src/components/pages/protected-page/Protected.jsx
+++ src/components/pages/protected-page/Protected.jsx
@@ -2,22 +2,31 @@ import { useContext } from "react";
 import { AuthContext } from "../../context-provider/AuthProvider";
 import PropTypes from 'prop-types';
 import { Navigate, useLocation } from "react-router-dom";
+import { BounceLoader } from "react-spinners";
 
-const Protected = ({children}) => {
+const Protected = ({ children }) => {
     const location = useLocation();
     const state = {
-        from : location.pathname
+        from: location.pathname
     };
-    const {loggedUser, isLoading} = useContext(AuthContext);
-    if(isLoading){
-        return <p>Loading</p>
+    const { loggedUser, isLoading } = useContext(AuthContext);
+    if (isLoading) {
+        return <div className="w-full h-screen flex justify-center items-center">
+            <BounceLoader
+                color="tomato"
+                size={80}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+                speedMultiplier={3}
+            />
+        </div>
     }
-    else if(!loggedUser){
-        return <Navigate to="/login" state={state} replace={true}/>
+    else if (!loggedUser) {
+        return <Navigate to="/login" state={state} replace={true} />
     }
     return children;
 };
 Protected.propTypes = {
-    children : PropTypes.element.isRequired
+    children: PropTypes.element.isRequired
 };
 export default Protected;
